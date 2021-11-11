57b0293997b8fab4fc91b21ee3244e55cc31ef77
arian457 <ariancejas00@gmail.com>
2021-11-10 23:33:21 -0300

    fixed connection with heroku DB

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require("dotenv").config();
 
 conn.sync().then(() => {
-  server.listen(3005, () => {
+  console.log($PORT || process.env.PORT)
+  server.listen(process.env.PORT || $PORT, () => {
     console.log('Everything Ready!');
   });
 });
