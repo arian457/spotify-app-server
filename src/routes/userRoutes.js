188533058f75aea1a34e1abd0e47f53c838b6cb3
arian457 <ariancejas00@gmail.com>
2021-11-10 22:02:14 -0300

    [Auth] Token refresh session controller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { registerController, loginController } = require('../controllers/auth');
+const { registerController, loginController, refreshSession } = require('../controllers/auth');
 const { registerValidations, loginValidations } = require('../middlewares/auth');
+const { tokenExists } = require('../middlewares/token');
 const router = Router();
 
 
 router.post("/register", registerValidations, registerController)
 router.post("/login", loginValidations, loginController)
+router.post("/me", tokenExists, refreshSession)
 
 module.exports = router;
